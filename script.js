14f95e12114ff5381a7f94b8b834467526506997
mike_visser <mpviss@gmail.com>
2020-07-06 11:28:25 -0400

    reset function and event listener added

--- script.js
+++ script.js
@@ -6,23 +6,26 @@ let blue = "blue"
 let currentPlayer = red
 
 let board = document.querySelector(".board")
+let footer = document.querySelector("footer")
 
-let tiles = [0,1,2,3,4,5,6,7,8]
+let tiles = []
 
 
 //Function to set tiles with class tile iterating over the array
 
 function createBoard() {
-    tiles.forEach(n => {
+    for (let i=0; i<9; i++) {
         let tile = document.createElement("div")
-        tile.setAttribute("id", `${n}`)
+        //tile.setAttribute("id", `${i}`)
         tile.classList.add("tile")
         tile.dataset.selected = false
         board.appendChild(tile)
-    })
+    }
 }
 
 createBoard()
+
+
 
 //Function to set clicked tile class of current player
 
@@ -34,6 +37,7 @@ function selectTile(e, currentPlayer) {
         console.log("false")
         e.target.classList.add(currentPlayer)
         e.target.dataset.selected = true
+        playerSwitch()
     }
     else {
         console.log("aready selected")
@@ -45,6 +49,30 @@ board.addEventListener("click", e =>{
     selectTile(e, currentPlayer)
     }
 })
-//Function to play round and change players
+
+//Function to switch players
+function playerSwitch () {
+    if (currentPlayer == "red"){
+        currentPlayer = blue
+    }
+    else {
+        currentPlayer = red
+    }
+}
 
 //Function to reset game
+function resetGame(){
+
+    tiles = document.querySelectorAll(".tile")
+    tiles.forEach(n =>{
+        n.setAttribute("class", "tile")
+        n.dataset.selected = "false"
+        currentPlayer = red
+    })
+}
+
+footer.addEventListener("click", e =>{
+    if(e.target.classList == "reset-button"){
+        resetGame()
+    }
+})
